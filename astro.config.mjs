554bfba88304d4f4edfe949f5327e61c984db7e9
Nathan Parker <nathan@ncp.dev>
2025-04-16 14:16:39 -0400

    :wrench: Table Layout + Fixes
    - Due to Xata downtime, the backend was haulted, on us-east-1
    
    - Changes made to the table layout, and there is now controls to control the course

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  integrations: [icon()],
 });
