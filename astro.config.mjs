69308af65da07d313f50421e9b5bfc13814d8888
Nathan Parker <nathan@ncp.dev>
2025-04-25 20:51:35 -0400

    :wrench: Basic Login
    - Added integration with Clerk for user login and session storage
    
    - Need to figure out 'roles' and block routes and actions through them

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
-
+import clerk from "@clerk/astro";
 import icon from 'astro-icon';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [icon()],
+  integrations: [clerk(), icon()],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="astro/client" />
+/// <reference types="@clerk/astro/env"/>
 
 interface ImportMetaEnv {
 	readonly XATA_API_KEY: string;
