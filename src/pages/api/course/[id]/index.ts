021c8dfeb603369dafb881d03e09900d468b56aa
pargonate <nathan@ncp.dev>
2025-04-27 03:45:22 -0400

    🚮 DELETE + PUT
    - Resurrected both API routes of DELETE and PUT
    - Fixed (in Vercel) the .env (so should have correct SERVER_URL)

--- src/pages/api/course/[id]/index.ts
+++ src/pages/api/course/[id]/index.ts
@@ -45,7 +45,7 @@ export const GET: APIRoute = async ({ params, request }) => {
 		});
 	}
 
-	const course = supabase.from("courses").select('course_id').eq("course_id", courseID);
+	const course = await supabase.from("courses").select('*').eq("course_id", courseID);
 
 	return new Response(JSON.stringify(course), {
 		status: 200,
@@ -53,4 +53,54 @@ export const GET: APIRoute = async ({ params, request }) => {
 			'Content-Type': 'application/json'
 		}
 	});
+}
+
+export const PUT: APIRoute = async ({ params, request }) => {
+	const courseID = params.id;
+
+	// If no ID is provided, cancel the request
+	if (!courseID) {
+		return new Response(JSON.stringify({error: 'courseID is required'}), {
+			status: 400,
+		});
+	}
+	
+	// Doesn't include creator_name because it would be illogical
+	// to change as then it wouldn't reflect the original creator.
+	const courseData = await request.json();
+	const course = await supabase.from("courses").update({
+		creator_id: courseData.creator_id,
+		course_id: courseID, 
+		name: courseData.name,
+		description: courseData.description,
+		subject: courseData.subject,
+		credits: courseData.credits		
+	}).eq('course_id', courseID);
+
+	return new Response(JSON.stringify(course), {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+}
+
+export const DELETE: APIRoute = async ({ params, request }) => {
+	const courseID = params.id;
+
+	// If no ID is provided, cancel the request
+	if (!courseID) {
+		return new Response(JSON.stringify({error: 'courseID is required'}), {
+			status: 400,
+		});
+	}
+	
+	const course = await supabase.from("courses").delete().eq('course_id', courseID);
+
+	return new Response(JSON.stringify(course), {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
 }
--- src/pages/api/course/index.ts
+++
@@ -1,5 +0,0 @@
-//
-// SUPABASE COURSE CREATION
-//
-//
-
